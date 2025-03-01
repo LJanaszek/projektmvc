1926c4338d4038ca225e79c5495f76f8c9638a15
LJanaszek <lukasjanaszek@gmail.com>
2025-03-01 18:32:51 +0100

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [data, setData] = useState("")
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((data) => setData(data.name))
+  })
+  console.log(data)
   return <Component {...pageProps} />;
 }
